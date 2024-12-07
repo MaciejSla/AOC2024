0f88a434f8e5aec2928327b6d54a973ab722277d
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-12-08 00:05:20 +0100

    Day7 Star2 done

--- Day7/main.go
+++ Day7/main.go
@@ -13,6 +13,7 @@ func main() {
 	lines := strings.Split(string(data), "\r\n")
 
 	sum := 0
+	sum2 := 0
 	for _, line := range lines {
 		split := strings.Split(line, ": ")
 		testVal, err := strconv.Atoi(split[0])
@@ -24,6 +25,8 @@ func main() {
 			utils.ThrowErr(err)
 			rest = append(rest, intVal)
 		}
+
+		// Star 1
 		solutions := []int{rest[0]}
 		for _, val := range rest[1:] {
 			tempSolutions := []int{}
@@ -36,6 +39,25 @@ func main() {
 		if slices.Contains(solutions, testVal) {
 			sum += testVal
 		}
+
+		// Star 2
+		solutions2 := []int{rest[0]}
+		for _, val := range rest[1:] {
+			tempSolutions := []int{}
+			for _, solution := range solutions2 {
+				tempSolutions = append(tempSolutions, solution*val)
+				tempSolutions = append(tempSolutions, solution+val)
+				concat, err := strconv.Atoi(fmt.Sprintf("%d%d", solution, val))
+				utils.ThrowErr(err)
+				tempSolutions = append(tempSolutions, concat)
+			}
+			solutions2 = tempSolutions
+		}
+		if slices.Contains(solutions2, testVal) {
+			sum2 += testVal
+		}
 	}
 	fmt.Println("Star1 sum: ", sum)
+	fmt.Println("Star2 sum: ", sum2)
+
 }

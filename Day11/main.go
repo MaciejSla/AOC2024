18796fbbb207d14ddf89d2b4675093250bebd378
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-12-11 23:20:06 +0100

    Day11 done

--- Day11/main.go
+++ Day11/main.go
@@ -19,32 +19,68 @@ func countDigits(stone int64) int {
 	return digitCount
 }
 
-func recurseBlink(timesLeft int, stone int64) int {
-	if timesLeft == 0 {
-		return 1
+// Previous (bruteforce) solution
+
+// func recurseBlink(timesLeft int, stone int64) int {
+// 	if timesLeft == 0 {
+// 		return 1
+// 	}
+
+// 	switch stone {
+// 	case 0:
+// 		return recurseBlink(timesLeft-1, 1)
+// 	default:
+// 		digits := countDigits(stone)
+// 		if digits%2 == 0 {
+// 			// Split the number
+// 			halfDigits := digits / 2
+// 			divisor := int64(math.Pow10(halfDigits))
+// 			part1 := stone / divisor
+// 			part2 := stone % divisor
+
+// 			return recurseBlink(timesLeft-1, part1) + recurseBlink(timesLeft-1, part2)
+// 		} else {
+// 			return recurseBlink(timesLeft-1, stone*2024)
+// 		}
+// 	}
+// }
+
+// func threadedWrap(timesLeft int, stone int64, c chan int) {
+// 	c <- recurseBlink(timesLeft, stone)
+// }
+
+func blink(stones []string, blinks int) int {
+	result := 0
+	stoneMap := make(map[int64]int)
+	for _, stone := range stones {
+		numStone, err := strconv.Atoi(stone)
+		utils.ThrowErr(err)
+		stoneMap[int64(numStone)] += 1
 	}
+	for range blinks {
+		newMap := make(map[int64]int)
+		for stone := range stoneMap {
+			digits := countDigits(int64(stone))
+			if stone == 0 {
+				newMap[1] += stoneMap[0]
+			} else if digits%2 == 0 {
+				halfDigits := digits / 2
+				divisor := int64(math.Pow10(halfDigits))
+				part1 := stone / divisor
+				part2 := stone % divisor
 
-	switch stone {
-	case 0:
-		return recurseBlink(timesLeft-1, 1)
-	default:
-		digits := countDigits(stone)
-		if digits%2 == 0 {
-			// Split the number
-			halfDigits := digits / 2
-			divisor := int64(math.Pow10(halfDigits))
-			part1 := stone / divisor
-			part2 := stone % divisor
-
-			return recurseBlink(timesLeft-1, part1) + recurseBlink(timesLeft-1, part2)
-		} else {
-			return recurseBlink(timesLeft-1, stone*2024)
+				newMap[part1] += stoneMap[stone]
+				newMap[part2] += stoneMap[stone]
+			} else {
+				newMap[int64(stone*2024)] = stoneMap[stone]
+			}
 		}
+		stoneMap = newMap
 	}
-}
-
-func threadedWrap(timesLeft int, stone int64, c chan int) {
-	c <- recurseBlink(timesLeft, stone)
+	for _, value := range stoneMap {
+		result += value
+	}
+	return result
 }
 
 func main() {
@@ -52,29 +88,12 @@ func main() {
 	stones := strings.Split(string(data), " ")
 
 	start := time.Now()
-	result := 0
-	c := make(chan int)
-	for _, stone := range stones {
-		numStone, err := strconv.Atoi(stone)
-		utils.ThrowErr(err)
-		go threadedWrap(25, int64(numStone), c)
-	}
-	for range len(stones) {
-		result += <-c
-	}
+	result := blink(stones, 25)
 	fmt.Println("Star1 count: ", result)
 	fmt.Println("Time 1: ", time.Since(start))
 
 	start = time.Now()
-	result = 0
-	for _, stone := range stones {
-		numStone, err := strconv.Atoi(stone)
-		utils.ThrowErr(err)
-		go threadedWrap(75, int64(numStone), c)
-	}
-	for range len(stones) {
-		result += <-c
-	}
+	result = blink(stones, 75)
 	fmt.Println("Star2 count: ", result)
 	fmt.Println("Time 2: ", time.Since(start))
 }
